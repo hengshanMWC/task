02831d46d2efd1715fee83bbae8289b834ba9c05
mawencan <1819293933@qq.com>
2023-02-14 00:36:46 +0800

    chore: 调整

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, './src/index.ts'),
-      formats: ['es'],
+      formats: ['es', 'umd'],
       fileName: 'index',
     },
   },
