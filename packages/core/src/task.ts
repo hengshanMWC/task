fc3d14dda686d04d41b80c5c1a18e60dd3385ce5
mawencan <1819293933@qq.com>
2023-02-15 01:08:04 +0800

    feat(core): 迭代

--- packages/core/src/task.ts
+++ packages/core/src/task.ts
@@ -2,7 +2,8 @@ import type { BaseTask, TaskStatus } from './core'
 import { CurrentPromise } from './core'
 abstract class Task<T = any> extends CurrentPromise implements BaseTask<T> {
   status: TaskStatus = 'idle'
-  ctx: T
+  ctx: T | undefined
+  sign = 0
 
   start(params?: T): Promise<any> {
     return this.createPromiseSingleton(params).currentPromise as Promise<any>
@@ -18,48 +19,15 @@ abstract class Task<T = any> extends CurrentPromise implements BaseTask<T> {
     return this.clear()
   }
 
-  reset() {
-    return this.cancel().start()
+  reset(params?: T) {
+    return this.cancel().start(params)
   }
 
   clear() {
+    this.ctx = undefined
     this.currentPromise = undefined
     this.currentReject = undefined
     this.currentResolve = undefined
-    return this
-  }
-
-  private createPromiseSingleton(params?: T) {
-    if (this.status === 'pause') {
-      this.status = 'active'
-      this.cut()
-    }
-    else if (this.status !== 'active') {
-      this.status = 'active'
-      this.currentPromise = new Promise((resolve, reject) => {
-        this.currentResolve = resolve
-        this.currentReject = reject
-        this.run(params).cut()
-      }).finally(() => {
-        this.clear()
-        this.status = 'end'
-      })
-    }
-    return this
-  }
-
-  protected next(end?: boolean) {
-    if (end === true) {
-      this.triggerResolve()
-    }
-    else if (this.status === 'active') {
-      try {
-        this.cut()
-      }
-      catch (err) {
-        this.triggerReject(err)
-      }
-    }
     return this
   }
 
@@ -69,9 +37,60 @@ abstract class Task<T = any> extends CurrentPromise implements BaseTask<T> {
     }
     return this
   }
-  protected abstract cut(): this
-}
 
+  private execute(next: Next<Task>, param?: NextParam) {
+    const end = typeof param === 'function' ? param() : param
+    if (end === true) {
+      this.triggerResolve()
+    }
+    else if (this.status === 'active') {
+      try {
+        this.cut(next)
+      }
+      catch (err) {
+        this.triggerReject(err)
+      }
+    }
+    return this
+  }
+
+  protected abstract cut(next: Next<Task>): this
+
+  private createPromiseSingleton(params?: T) {
+    if (this.status === 'pause') {
+      this.status = 'active'
+      const next = this.createNext()
+      this.cut(next)
+    }
+    else if (this.status !== 'active') {
+      this.status = 'active'
+      const next = this.createNext()
+      this.currentPromise = new Promise((resolve, reject) => {
+        this.currentResolve = resolve
+        this.currentReject = reject
+
+        this.run(params).cut(next)
+      }).finally(() => {
+        this.clear()
+        this.status = 'end'
+      })
+    }
+    return this
+  }
+
+  private createNext() {
+    const sign = ++this.sign
+    const next: Next<Task> = (param) => {
+      if (sign === this.sign) {
+        this.execute(next, param)
+      }
+      return this
+    }
+    return next
+  }
+}
+type NextParam = boolean | (() => boolean)
+type Next<T> = (param?: NextParam) => T
 export {
   Task,
 }
--- packages/core/test/specs/index.test.ts
+++ packages/core/test/specs/index.test.ts
@@ -1,23 +1,31 @@
 import { describe, expect, test } from 'vitest'
 import { Task } from '../../src/index'
+import { wait } from '../utils'
+const value = 2
+
 class TestTask extends Task<number> {
-  cut() {
-    if (this.ctx) {
-      Promise.resolve()
-        .then(() => {
-          this.ctx--
-          this.next()
-        })
-      return this
+  cut(next) {
+    if (this.ctx === value + 1) {
+      throw new Error('test')
     }
     else {
-      return this.next(true)
+      Promise.resolve()
+        .then(() => {
+          next(() => {
+            if (this.ctx) {
+              this.ctx--
+            }
+            else {
+              return true
+            }
+          })
+        })
     }
+    return this
   }
 }
 describe('test', () => {
-  test('base', async () => {
-    const value = 2
+  test('start->pause->start', async () => {
     const task = new TestTask()
     expect(task.status).toBe('idle')
     const p = task.start(value)
@@ -29,11 +37,37 @@ describe('test', () => {
     expect(task.status).toBe('active')
     // 测试暂停
     task.pause()
-    await Promise.resolve()
+    await wait()
     expect(task.status).toBe('pause')
     expect(value - 1).toBe(task.ctx)
     // 测试重新启动
     expect(p).toBe(task.start())
     expect(task.status).toBe('active')
+  })
+  test('cancel', () => {
+    const task = new TestTask()
+    task.start(value)
+    task.cancel()
+    expect(task.ctx).toBeUndefined()
+    expect(task.status).toBe('end')
+  })
+  test('reset', async () => {
+    const task = new TestTask()
+    const p = task.start(value)
+    const p2 = task.reset(value)
+    expect(p).not.toBe(p2)
+    await Promise.resolve()
+    expect(value - 1).toBe(task.ctx)
+  })
+  test('error', async () => {
+    const task = new TestTask()
+    const fn = vi.fn()
+    try {
+      await task.start(value + 1)
+    }
+    catch {
+      fn()
+    }
+    expect(fn).toHaveBeenCalled()
   })
 })
