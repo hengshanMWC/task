292df47412a244bac4e17e8243d5fba342856787
mawencan <1819293933@qq.com>
2023-02-14 21:58:37 +0800

    test(core): 添加Task测试

--- packages/core/test/specs/index.test.ts
+++ packages/core/test/specs/index.test.ts
@@ -1,24 +1,39 @@
 import { describe, expect, test } from 'vitest'
 import { Task } from '../../src/index'
-import { wait } from '../utils'
-describe('test', () => {
-  test('demo', async () => {
-    class TestTask extends Task {
-      cut() {
-        return this.next(true)
-      }
-
-      run() {
-        return this
-      }
+class TestTask extends Task<number> {
+  cut() {
+    if (this.ctx) {
+      Promise.resolve()
+        .then(() => {
+          this.ctx--
+          this.next()
+        })
+      return this
     }
+    else {
+      return this.next(true)
+    }
+  }
+}
+describe('test', () => {
+  test('base', async () => {
+    const value = 2
     const task = new TestTask()
     expect(task.status).toBe('idle')
-    const p = task.start()
+    const p = task.start(value)
+    p.then(() => {
+      expect(task.status).toBe('end')
+    })
+    // 测试是否同一个Promise
     expect(p).toBe(task.start())
     expect(task.status).toBe('active')
-    await wait(1000)
+    // 测试暂停
+    task.pause()
+    await Promise.resolve()
+    expect(task.status).toBe('pause')
+    expect(value - 1).toBe(task.ctx)
+    // 测试重新启动
     expect(p).toBe(task.start())
-    expect(task.status).toBe('end')
+    expect(task.status).toBe('active')
   })
 })
